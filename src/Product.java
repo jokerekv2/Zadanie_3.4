6c32c7d63c49ab231dd8b1202f2b34e0b39d89fa
Dominik Mikolajczyk <jokerekv2@gmail.com>
2020-10-09 19:20:05 +0200

    Solution

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,32 @@
 public class Product {
+    String name;
+    double price;
+    String description;
+    Category category;
+
+    public Product(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public Product(String name, double price, String description, Category category) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        if (category == null) {
+            return "Name: " + name +
+                    ", price: " + price +
+                    ", description: " + description + "\n";
+        } else
+            return "Name: " + name +
+                    ", price: " + price +
+                    ", description: " + description +
+                    ", category: " + category.toString() + "\n";
+    }
 }
--- src/SpecialOffer.java
+++ src/SpecialOffer.java
@@ -1,2 +1,25 @@
 public class SpecialOffer {
+    Product specialProduct;
+    String offer;
+    String time;
+    double discount;
+    double priceAfterDiscount;
+
+    public SpecialOffer(Product specialProduct, String offer, String time, double discount) {
+        this.specialProduct = specialProduct;
+        this.offer = offer;
+        this.time = time;
+        this.discount = discount;
+        this.priceAfterDiscount = afterDiscount();
+    }
+
+    public double afterDiscount() {
+        return (1 - 0.2) * specialProduct.price;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + specialProduct.name + ", normal price: " + specialProduct.price + ", price after discount: "
+                + priceAfterDiscount + ", description: " + specialProduct.description + ", Offer: " + offer + ", " + time;
+    }
 }
